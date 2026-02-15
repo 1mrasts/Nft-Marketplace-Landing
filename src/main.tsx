a6cf11c96335ae444c63b2e91f7bf1bb53f17b58
1mrasts <olgaakshidi.ru@gmail.com>
2026-02-15 13:43:36 +0300

    Update Router import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
-import { router } from './app/router'
+import { Router } from './app/routing'
 
 createRoot(document.getElementById('root')!).render(
-	<RouterProvider router={router} />,
+	<RouterProvider router={Router} />,
 )
