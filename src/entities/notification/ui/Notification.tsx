2d4349fb2edf08bc139d711cce1c221008562a33
1mrasts <olgaakshidi.ru@gmail.com>
2026-02-22 21:23:15 +0300

    Rework Notification component layout

--- src/entities/notification/ui/Notification.tsx
+++ src/entities/notification/ui/Notification.tsx
@@ -5,6 +5,7 @@ import type { userType } from '../../users/model/types'
 import { getNotificationText } from '../model/getNotificationText'
 import { notificationMockData } from '../model/mockData'
 import type { notification } from '../model/types'
+import verifiedArt from '../verified-artist.svg'
 import styles from './Notification.module.scss'
 
 export function Notification({ id }: { id: number }) {
@@ -20,17 +21,20 @@ export function Notification({ id }: { id: number }) {
 
 	return (
 		<div className={styles.notification}>
-			<div className={styles['notification__avatar']}>
-				<div className='circle'></div>
-				<img src={user.avatar} alt='' />
+			<div className={styles['notification__body']}>
+				<div className={styles['notification__avatar']}>
+					<div className='circle'></div>
+					<div className={styles['notification__icon']}>
+						<img className={styles.verified} src={verifiedArt} alt='' />
+						<img src={user.avatar} alt='' />
+					</div>
+				</div>
+				<div className={styles['notification__text']}>
+					{getNotificationText(notification, user, nft)}
+					<span>{notification.time}</span>
+				</div>
 			</div>
-			<div className={styles['notification__text']}>
-				{getNotificationText(notification, user, nft)}
-				<span>{notification.time}</span>
-			</div>
-			<div className={styles['notification__image']}>
-				<img src={nft.image} alt='' />
-			</div>
+			<img className={styles['notification__image']} src={nft.image} alt='' />
 		</div>
 	)
 }
