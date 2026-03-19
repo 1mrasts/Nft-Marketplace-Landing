ae4879516551ebe245225cbf59f77e0464c50869
1mrasts <olgaakshidi.ru@gmail.com>
2026-03-19 22:20:16 +0300

    Fix bug

--- src/widgets/ProductNft/ui/ProductNft.tsx
+++ src/widgets/ProductNft/ui/ProductNft.tsx
@@ -1,3 +1,8 @@
-export function ProductNft({ id }: { id: number }) {
-	return <>{/* <div className={styles.product}></div> */}</>
+import { useNfts } from '../../../entities/nfts/model/useNfts'
+
+export function ProductNft({ id }: { id: string | undefined }) {
+	const nft = useNfts().nfts.filter(item => item.id == Number(id))[0]
+	console.log(id)
+	console.log(nft.id)
+	return <div>{nft.name}</div>
 }
