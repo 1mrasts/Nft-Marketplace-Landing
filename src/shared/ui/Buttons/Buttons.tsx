f617dabb8db2800b6ef543cb08dcaff14653461b
1mrasts <olgaakshidi.ru@gmail.com>
2026-03-25 19:07:32 +0300

    Refactor slider logic in TopNFT component

--- src/shared/ui/Buttons/Buttons.tsx
+++ src/shared/ui/Buttons/Buttons.tsx
@@ -1,35 +1,17 @@
 import type { MouseEventHandler } from 'react'
 
 export function Button({
-	type,
 	switchFunction,
-	index,
-	length,
 	arrowActive,
-	arrowInactive,
 }: {
-	type: string
 	switchFunction: MouseEventHandler<HTMLButtonElement>
-	index: number
-	length: number
+
 	arrowActive: string
-	arrowInactive: string
 }) {
 	return (
 		<>
 			<button onClick={switchFunction}>
-				<img
-					src={
-						type == 'next'
-							? index >= Math.round(length / 2) - 2
-								? arrowInactive
-								: arrowActive
-							: index <= Math.round((length / 2) * -1) + 2
-								? arrowInactive
-								: arrowActive
-					}
-					alt=''
-				/>
+				<img src={arrowActive} alt='' />
 			</button>
 		</>
 	)
