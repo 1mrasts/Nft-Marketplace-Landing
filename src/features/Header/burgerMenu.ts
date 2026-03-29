2400a39c574df09b5ba3d96f7e5544c992d7364f
1mrasts <olgaakshidi.ru@gmail.com>
2026-03-29 17:50:13 +0300

    Remove comments and logging

--- src/features/Header/burgerMenu.ts
+++ src/features/Header/burgerMenu.ts
@@ -1,7 +1,5 @@
 export function burgerClickHandle(
 	setHideToggle: React.Dispatch<React.SetStateAction<string>>,
 ) {
-	// TODO: сделать логику появления меню по нажатию на burger
-	console.log('Бургер меню открыто')
 	setHideToggle(prev => (prev == '' ? 'open' : ''))
 }
