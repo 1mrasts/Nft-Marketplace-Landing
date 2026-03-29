f5ef9c29638125c07106a0334e8989ff192a762c
1mrasts <olgaakshidi.ru@gmail.com>
2026-03-29 17:43:10 +0300

    Refactor header component for responsive design

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -10,49 +10,12 @@ export function Header() {
 
 	return (
 		<header className={`${styles.header} wrapper`}>
-			<div className={styles['header__nav']}>
-				<Link to='/'>
-					<img src={Logo} alt='Логотип' />
-				</Link>
-				<nav id='adaptive-hide'>
-					<ul>
-						<li>
-							<Link to='discover'>DISCOVER</Link>
-						</li>
-						<div className='sep-horiz'></div>
-						<li>
-							<Link to='creators'>CREATORS</Link>
-						</li>
-						<div className='sep-horiz'></div>
-						<li>
-							<Link to='sell'>SELL</Link>
-						</li>
-						<div className='sep-horiz'></div>
-						<li>
-							<Link to='stats'>STATS</Link>
-						</li>
-					</ul>
-				</nav>
-			</div>
-			<div id='adaptive-hide' className={`${styles['header__login']} button`}>
-				<input type='search' placeholder='Search Art Work / Creator' />
-				<button>CONNECT WALLET</button>
-			</div>
-			<div id='adaptive-show' className={styles['burger__block']}>
-				<img
-					id={styles['burger']}
-					src={burger}
-					alt=''
-					onClick={() => burgerClickHandle(setHideToggle)}
-				/>
-				<div
-					className={
-						hideToggle.length < 1
-							? styles['burger__block-menu']
-							: `${styles['burger__block-menu']} ${styles.open}`
-					}
-				>
-					<nav>
+			<div className={styles['header__block']}>
+				<div className={styles['header__nav']}>
+					<Link to='/'>
+						<img src={Logo} alt='Логотип' />
+					</Link>
+					<nav id='adaptive-hide'>
 						<ul>
 							<li>
 								<Link to='discover'>DISCOVER</Link>
@@ -72,7 +35,47 @@ export function Header() {
 						</ul>
 					</nav>
 				</div>
+				<div id='adaptive-hide' className={`${styles['header__login']} button`}>
+					<input type='search' placeholder='Search Art Work / Creator' />
+					<button>CONNECT WALLET</button>
+				</div>
+				<div id='adaptive-show' className={styles['burger__block']}>
+					<img
+						id={styles['burger']}
+						src={burger}
+						alt=''
+						onClick={() => burgerClickHandle(setHideToggle)}
+					/>
+					<div
+						className={
+							hideToggle.length < 1
+								? styles['burger__block-menu']
+								: `${styles['burger__block-menu']} ${styles.open}`
+						}
+					>
+						<nav>
+							<ul>
+								<li>
+									<Link to='discover'>DISCOVER</Link>
+								</li>
+								<div className='sep-horiz'></div>
+								<li>
+									<Link to='creators'>CREATORS</Link>
+								</li>
+								<div className='sep-horiz'></div>
+								<li>
+									<Link to='sell'>SELL</Link>
+								</li>
+								<div className='sep-horiz'></div>
+								<li>
+									<Link to='stats'>STATS</Link>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
 			</div>
+			<div id='adaptive-show' className='sep-horiz-1px'></div>
 		</header>
 	)
 }
