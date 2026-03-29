cd6348d82742dfdcc1a33e511fb01e4c812b7d96
1mrasts <olgaakshidi.ru@gmail.com>
2026-03-29 17:43:32 +0300

    Refactor hero component for responsive design

--- src/widgets/Hero/ui/Hero.tsx
+++ src/widgets/Hero/ui/Hero.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router'
 import heroBanner from '../Hero-banner.png'
+import trust from '../Trust Reviews.png'
 import styles from './Hero.module.scss'
 
 export function Hero() {
@@ -7,10 +8,16 @@ export function Hero() {
 
 	return (
 		<section className={`${styles.hero} wrapper`}>
-			<img src={heroBanner} alt='Header' />
 			<div className={styles['hero__text']}>
-				<div>
-					<h1>Discover And Create NFTs</h1>
+				<div className={styles['hero__desc']}>
+					<div id='adaptive-show' className={styles['creators__text']}>
+						<div className={styles['sep-horiz']}></div>
+						<p>OVER 1M CREATORS</p>
+					</div>
+					<h1>
+						Discover And
+						<br /> Create NFTs
+					</h1>
 					<p>
 						Discover, Create and Sell NFTs On Our NFT Marketplace With Over
 						Thousands Of NFTs And Get a <span>$20 bonus.</span>
@@ -26,6 +33,8 @@ export function Hero() {
 					</button>
 				</div>
 			</div>
+			<img src={trust} alt='' className='mt-20' />
+			<img className={styles['hero__banner']} src={heroBanner} alt='Header' />
 		</section>
 	)
 }
